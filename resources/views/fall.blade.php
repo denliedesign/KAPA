d4189002cdf2c6f8281ac1d8067f2790394351f4
denliedesign <customdenlie@gmail.com>
2023-08-15 22:28:55 -0500

    fall sched

--- resources/views/fall.blade.php
+++ resources/views/fall.blade.php
@@ -11,19 +11,19 @@
 
     <div class="bg-white">
         <div class="container">
-            <h3 class="text-center pt-5">Fall Schedule 2021</h3>
+            <h3 class="text-center pt-5">2023 Fall Schedule</h3>
             <div class="text-center d-flex d-inline justify-content-center">
-                <div class="mx-2 px-2"><a href="/images/fall-schedule.pdf" target="_blank"><button class="btn shadow btn-kapa-orange btn-lg">View Fall Schedule PDF</button></a></div>
+                <div class="mx-2 px-2"><a href="/images/23-fall.pdf" target="_blank"><button class="btn shadow btn-kapa-orange btn-lg">View Fall Schedule PDF</button></a></div>
                 <div class="mx-2 px-2"><a href="https://app.jackrabbitclass.com/regv2.asp?id=504663" target="_blank"><button class="btn shadow btn-kapa-pink btn-lg">Enroll Now!</button></a></div>
             </div>
-            <img src="images/fall-schedule-1.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-2.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-3.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-4.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-5.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-6.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-7.jpg" alt="fall schedule" class="img-thumbnail my-3">
-            <img src="images/fall-schedule-8.jpg" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-1.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-2.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-3.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-4.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-5.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-6.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-7.png" alt="fall schedule" class="img-thumbnail my-3">
+            <img src="images/23-fall-8.png" alt="fall schedule" class="img-thumbnail my-3">
         </div>
     </div>
 
